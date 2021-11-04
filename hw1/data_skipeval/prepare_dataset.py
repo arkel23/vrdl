921d5c995f23c00a5b3242d8fbd28b330920990d
edwin_pcs05 <edwin-2310@hotmail.com>
2021-11-04 09:48:55 -0400

    pep8

--- hw1/data_skipeval/prepare_dataset.py
+++ hw1/data_skipeval/prepare_dataset.py
@@ -4,8 +4,8 @@ import pandas as pd
 
 def create_categorical():
     df = pd.read_csv('training_labels.txt', sep=' ', index_col=False,
-    header=None, names=['file_name', 'class_name'], 
-    dtype={'file_name': 'object', 'class_name': 'category'})
+                     header=None, names=['file_name', 'class_name'],
+                     dtype={'file_name': 'object', 'class_name': 'category'})
 
     df['class_id'] = df['class_name'].cat.codes
 
@@ -22,23 +22,26 @@ def create_categorical():
 def split_train_val(split_percent):
     df = pd.read_csv('train_val.csv')
     print('Original train-val split\n', df.head(), '\n', df.shape)
-    
+
     samples_per_class_df = df.groupby('class_id', as_index=True).count()
-    
+
     df_list_train = []
     df_list_val = []
-    for class_id, total_samples_class in enumerate(samples_per_class_df['file_name']):
+    for class_id, total_samples_class in enumerate(
+            samples_per_class_df['file_name']):
         train_samples_class = int(total_samples_class*split_percent[0])
         val_samples_class = total_samples_class - train_samples_class
-        assert(train_samples_class+val_samples_class==total_samples_class)
-        train_subset_class = df.loc[df['class_id']==class_id].groupby('class_id').head(train_samples_class)
-        val_subset_class = df.loc[df['class_id']==class_id].groupby('class_id').tail(val_samples_class)
+        assert(train_samples_class+val_samples_class == total_samples_class)
+        train_subset_class = df.loc[df['class_id'] == class_id].groupby(
+            'class_id').head(train_samples_class)
+        val_subset_class = df.loc[df['class_id'] == class_id].groupby(
+            'class_id').tail(val_samples_class)
         df_list_train.append(train_subset_class)
         df_list_val.append(val_subset_class)
-    
+
     df_train = pd.concat(df_list_train)
     df_val = pd.concat(df_list_val)
-    
+
     print('Train df: ')
     print(df_train.head())
     print(df_train.shape)
@@ -52,16 +55,16 @@ def split_train_val(split_percent):
     df_val_name = 'val.csv'
     df_val.to_csv(df_val_name, sep=',', header=True, index=False)
     print('Finished saving train and val split dictionaries.')
-    
+
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--train_percent', type=float, default=0.7)
     parser.add_argument('--val_percent', type=float, default=0.3)
     args = parser.parse_args()
-    
+
     split_percent = [args.train_percent, args.val_percent]
-        
+
     create_categorical()
     split_train_val(split_percent)
 
